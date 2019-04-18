af62a595125389608f4d0870f864410cdfc11b19
trudla <gunka.magda@gmail.com>
2019-04-18 09:20:40 +0400

    added css to html file and js

---
+++ simpleConnectedDeviceAssignment/src/newServer.js
@@ -0,0 +1,98 @@
+/*
+	UDP And HTTP Server
+	Context: node.js
+
+	Serve a web page to a browser with a control panel
+	Read control panel and send results to Arduino via UDP
+
+	Use webSockets between http client and server
+
+	created 7 March 2019
+	by Michael Shiloh
+*/
+
+/* UDP server talks to Arduino */
+var dgram = require('dgram');
+var udpServer = dgram.createSocket('udp4')
+var ARDUINO_UDP_PORT = 2390;
+var ARDUINO_LISTENING_AT= 5000;
+var ARDUINO_ADDRESS = '192.168.1.18';
+
+/* HTTP server talks to browser */
+const http = require('http')
+const express = require('express'); // web server application
+const app = express();        // instantiate express server
+const httpServer = http.Server(app);  // connects http library to server
+const HTTP_SERVER_PORT = 8000; 
+
+// Express creates the simple web page
+// The argument says where to find pages and scripts
+app.use(express.static('public'));  
+
+// websockets so that webpage can talk back to server
+const webSocket = require('socket.io')(httpServer);  
+
+//send to website
+var recMsg;
+
+/* UDP server callback functions */
+
+function UDPServerIsListening() {
+	console.log('UDP Server is listening');
+}
+
+function UDPServerReceivedMessage(message, sender) {
+
+	// print the message
+	console.log(
+		'Received message from: ' +
+		sender.address + 
+		':' + 
+		sender.port);
+	console.log(
+		'Message length: ' +
+		message.length + 
+		' Message contents: ' +message.readUInt8(0));
+	recMsg = message.readUInt8(0);
+  	webSocket.emit('reading', recMsg);
+}
+
+/* Register the UDP callback functions */
+udpServer.bind(ARDUINO_UDP_PORT );
+udpServer.on('listening', UDPServerIsListening);
+udpServer.on('message', UDPServerReceivedMessage);
+
+/* HTTP callback functions */
+
+httpServer.listen(HTTP_SERVER_PORT, () => {
+	console.log('httpServer: Listening at', httpServer.address());
+});
+
+httpServer.on('connection', () => {
+  console.log("httpServer: An HTTP client has connected")
+});
+
+
+/* and here is the websocket event handler */
+
+webSocket.on('connect', (socket) => {
+	var getMoistureMessage = new Buffer('1');
+	console.log('Web server socket: Client connected');
+	// if you get the 'ledON' message
+	socket.on('getMoisture', () => {
+		console.log('Web server socket: received getMoisture message from web client');
+	    // Send the message to Arduino
+	    udpServer.send(getMoistureMessage, 
+			0, 
+			getMoistureMessage.length, 
+			ARDUINO_LISTENING_AT, 
+			ARDUINO_ADDRESS);
+	});
+
+    // if you get the 'disconnect' message, say the user disconnected
+	socket.on('disconnect', () => {
+    	console.log('Web server socket: user disconnected');
+	});
+});
+
+
--- simpleConnectedDeviceAssignment/src/public/client.js
+++ simpleConnectedDeviceAssignment/src/public/client.js
@@ -1,3 +1,45 @@
 /* 
-function for showing available data
+This is the websocket library which will allow us to send messages
+back to the web server 
 */
+var socket = io();
+var selectedVal;
+
+function getMoisture() {
+  console.log ("sending a message to the web server to get the moisture level");
+  // this is how we send a message back to the web server
+  socket.emit('getMoisture'); 
+};
+
+function GetSelectedItem(perct)
+{
+    var p = document.getElementById(perct);
+    console.log(p.options[p.selectedIndex].value);
+    selectedVal = p.options[p.selectedIndex].value;
+    data_first.style.display = 'none';
+    actMLvl.style.display='block';
+    data_second.style.display = 'block';
+    document.body.style.setProperty("--main-bg", "#f4e842");
+}
+
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
+socket.on('reading', function(values) {
+	var data = values;
+	document.getElementById("reading").innerHTML = data;
+	document.getElementById("perc_sign").innerHTML = "%";
+	//document.location.reload(false);
+	console.log(data);
+	console.log(selectedVal);
+	decision.style.display = 'block';
+	if (data > selectedVal){
+		document.body.style.setProperty("--main-bg", "#84f94d");
+		document.getElementById("decision").innerHTML = "No need to water the plant YET";
+	}
+	else {
+		document.body.style.setProperty("--main-bg", "#f44141");
+		document.getElementById("decision").innerHTML = "Water your plant if you don't want to kill it";
+	}
+});
